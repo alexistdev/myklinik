eb05457c6b0311a335a7548132b0ad4dc3fba5e4
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2024-03-07 17:05:42 +0700

    feat(data-pasien): create pasien model and migration

---
+++ WEB/myklinik/app/Models/Pasien.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Copyright (c) 2024.
+ * Develop By: Alexsander Hendra Wijaya
+ * Github: https://github.com/alexistdev
+ * Phone : 0823-7140-8678
+ * Email : Alexistdev@gmail.com
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Pasien extends Model
+{
+    use SoftDeletes;
+
+    protected $table = "pasiens";
+    protected $fillable = ['user_id','kode_pasien','tanggal_lahir','tempat_lahir','sex','agama','pendidikan','phone','gol_darah','pekerjaan','alamat'];
+}
---
+++ WEB/myklinik/database/migrations/2024_03_07_095528_create_pasiens_table.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Copyright (c) 2024.
+ * Develop By: Alexsander Hendra Wijaya
+ * Github: https://github.com/alexistdev
+ * Phone : 0823-7140-8678
+ * Email : Alexistdev@gmail.com
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pasiens', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('kode_pasien');
+            $table->date('tanggal_lahir');
+            $table->string('tempat_lahir');
+            $table->string('sex',1);
+            $table->string('agama');
+            $table->string('pendidikan');
+            $table->string('phone');
+            $table->string('gol_darah',2);
+            $table->string('pekerjaan');
+            $table->string('alamat');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pasiens');
+    }
+};
